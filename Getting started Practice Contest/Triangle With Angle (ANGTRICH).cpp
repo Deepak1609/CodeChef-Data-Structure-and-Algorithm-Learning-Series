66c7e7485c27176dd678710f009ca49f9fe77c74
Deepak <54376136+Deepak1609@users.noreply.github.com>
2021-10-02 11:04:42 +0530

    Triangle With Angle (ANGTRICH) in c++

---
+++ Getting started Practice Contest/Triangle With Angle (ANGTRICH).cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a,b,c;
+	cin>>a>>b>>c;
+	if((a+b>c || b+c>a || c+a>b) && (a+b+c==180) && (a>0 && b>0 && c>0))
+	      cout<<"YES"<<endl;
+	else
+	      cout<<"NO"<<endl;
+	return 0;
+}
