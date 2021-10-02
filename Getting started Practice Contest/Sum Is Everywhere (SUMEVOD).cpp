49cf28dc1d91a1512fdfee493ffd78c67f8acfea
Deepak <54376136+Deepak1609@users.noreply.github.com>
2021-10-02 11:03:37 +0530

    Sum Is Everywhere (SUMEVOD) in c++

---
+++ Getting started Practice Contest/Sum Is Everywhere (SUMEVOD).cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	long int n, sumO=0,sumE=0;
+	cin>>n;
+	for(int i =1; i<=n*2;i++ )
+	{
+	    if(i%2==0)
+	        sumE+=i;
+	    if(i%2!=0)
+	        sumO+=i;
+	}
+	cout<<sumO<<" "<<sumE<<endl;
+	return 0;
+}
