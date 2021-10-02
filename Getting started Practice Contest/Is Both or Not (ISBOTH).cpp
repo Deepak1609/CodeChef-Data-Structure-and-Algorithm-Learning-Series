a65f496a75d921d3fe0b0b646972b1c71a6493e2
Deepak <54376136+Deepak1609@users.noreply.github.com>
2021-10-02 10:46:26 +0530

    Is Both or Not (ISBOTH) in c++

---
+++ Getting started Practice Contest/Is Both or Not (ISBOTH).cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	if(n%5 == 0 && n%11 == 0)
+	       cout<<"BOTH"<<endl;
+	if((n%5 == 0 || n%11 == 0) && (n%5!=0 || n%11!=0))
+	       cout<<"ONE"<<endl;
+	if(n%5!=0 && n%11!=0 )
+	       cout<<"NONE"<<endl;
+	return 0;
+}
