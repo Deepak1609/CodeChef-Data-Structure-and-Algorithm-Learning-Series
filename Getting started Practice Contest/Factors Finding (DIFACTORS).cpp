55a219a33d1c2e7a2b757e9cfc92defab3b5a7c4
Deepak <54376136+Deepak1609@users.noreply.github.com>
2021-10-02 10:48:57 +0530

    Factors Finding (DIFACTORS) in c++

---
+++ Getting started Practice Contest/Factors Finding (DIFACTORS).cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    int n,c=0,j=0;
+    cin>>n;
+    long int a[1000000];
+    for(int i=1; i<=n; i++)
+    {
+        if(n%i==0)
+        {
+            c++;
+            a[j]=i;
+            j++;
+        }
+    }
+    cout<<c<<endl;
+    for(j = 0;j<c;j++)
+    {
+        cout<<a[j]<<" ";
+    }
+    return 0;
+}
