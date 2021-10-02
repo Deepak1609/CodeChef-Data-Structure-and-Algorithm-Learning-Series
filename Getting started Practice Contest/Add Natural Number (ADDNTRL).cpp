098c0acff4f43351a44f8dea622cd92cdc174a4e
Deepak <54376136+Deepak1609@users.noreply.github.com>
2021-10-02 11:00:43 +0530

    Add Natural Number (ADDNTRL) in c++

---
+++ Getting started Practice Contest/Add Natural Number (ADDNTRL).cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	long long int sum = 0, n;
+	cin>>n;
+	for(int i =1; i<=n; i++)
+	{
+	    sum += i;
+	}
+	cout<<sum<<endl;
+	return 0;
+}
