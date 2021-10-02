534b4f2f22e12403e70dbf88c57d0a5b3734a07e
Deepak <54376136+Deepak1609@users.noreply.github.com>
2021-10-02 11:05:51 +0530

    Triangle Everywhere (EXTRICHK) in c++

---
+++ Getting started Practice Contest/Triangle Everywhere (EXTRICHK).cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a,b,c;
+	cin>>a>>b>>c;
+	if(a+b>c && b+c>a && c+a>b)
+	{
+	    if(a==b && b==c && c==a)
+	         cout<<"1"<<endl;
+	    else if (a==b || b==c || c==a)
+	         cout<<"2"<<endl;
+	    else if(a!=b && b!=c && c!=a)
+	         cout<<"3"<<endl;
+	}
+	else
+	  cout<<"-1"<<endl;
+	return 0;
+}
