86e8437e7f1552e63aa7ca6929af73d39a9fe5a8
Deepak <54376136+Deepak1609@users.noreply.github.com>
2021-10-02 11:07:50 +0530

    Alternative Square Pattern (SQLPAT) in c++
    You're given a number N. Print the first N lines of the below-given pattern.
    
    1 2 3 4 5
    10 9 8 7 6
    11 12 13 14 15
    20 19 18 17 16
    21 22 23 24 25
    30 29 28 27 26
    Input:
    First-line will contain the number N.
    Output:
    Print the first N lines of the given pattern.
    
    Constraints
    1≤N≤200
    Sample Input 1:
    4
    Sample Output 1:
    1 2 3 4 5
    10 9 8 7 6
    11 12 13 14 15
    20 19 18 17 16
    Sample Input 2:
    2
    Sample Output 2:
    1 2 3 4 5
    10 9 8 7 6
    EXPLANATION:
    In the first example, we'll print the first 4 lines of the given pattern.
    In the second example, we'll print the first 2 lines of the given pattern.

---
+++ Getting started Practice Contest/Alternative Square Pattern (SQLPAT).cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	int count=0;
+    for(int i=1; i<=n; i++)
+    {
+        if(i%2!=0)
+        {
+            for(int j=1; j<=5; j++)
+            {
+                count = count +1;
+                cout<<count<<" ";
+            }
+        }
+        else
+        {
+            int temp = count+1;
+            for(int j=count+5; j>=temp; j--)
+            {
+                count=count+1;
+                cout<<j<<" ";
+            }
+            
+        }
+        cout<<endl;
+    }
+
+	return 0;
+}
